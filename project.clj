88b81af98b2c8dcc445cb29d182ad32b8083ac50
GBett <gilbertkalo@yahoo.com>
2022-12-18 15:48:16 +0300

    first commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject clojure.jdbc/clojure.jdbc-c3p0 "0.3.5-SNAPSHOT"
+  :description "c3p0, a mature, highly concurrent JDBC connection pooling library for clojure.jdbc"
+  :url "https://github.com/bostonaholic/clojure.jdbc-c3p0"
+  :license {:name "Apache 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.mchange/c3p0 "0.9.5.4"]]
+
+  :profiles {:dev {:dependencies [[clojure.jdbc "0.4.0"]
+                                  [com.h2database/h2 "1.4.196"]]}
+             :test {:jvm-opts ["-Dcom.mchange.v2.log.MLog=com.mchange.v2.log.FallbackMLog"
+                               "-Dcom.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL=OFF"]}})
---
+++ test/c3p0_tests.clj
@@ -0,0 +1,15 @@
+(ns c3p0-tests
+  (:require [jdbc.core :refer :all]
+            [jdbc.pool.c3p0 :as pool]
+            [clojure.test :refer :all]))
+
+(def dbspec {:subprotocol "h2"
+             :subname "/tmp/test"})
+
+(deftest datasource-spec
+  (testing "Connection pool testing."
+    (let [spec (pool/make-datasource-spec dbspec)]
+      (is (instance? javax.sql.DataSource (:datasource spec)))
+      (with-open [conn (make-connection spec)]
+        (let [result (query conn ["SELECT 1 + 1 as foo;"])]
+          (is (= [{:foo 2}] result)))))))
